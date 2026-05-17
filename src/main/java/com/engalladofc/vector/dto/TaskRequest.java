378ba65bd3a77c59ca0be270f03fa4d813c00b29
FredrickChris <engalladofc@gmail.com>
2026-05-17 19:51:04 +0800

    Task Request class added

---
+++ src/main/java/com/engalladofc/vector/dto/TaskRequest.java
@@ -0,0 +1,12 @@
+package com.engalladofc.vector.dto;
+
+import com.engalladofc.vector.model.Status;
+
+public class TaskRequest {
+    public String title;
+    public String subject;
+    public String description;
+    public String deadline;
+    public Integer difficulty;
+    public Status status;
+}
