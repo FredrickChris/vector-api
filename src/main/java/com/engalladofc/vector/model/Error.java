7bd1426728c80dd668de3067bf66a06b6884b747
FredrickChris <engalladofcm@gmail.com>
2026-05-18 17:15:42 +0800

    Removed Error indices

--- src/main/java/com/engalladofc/vector/model/Error.java
+++ src/main/java/com/engalladofc/vector/model/Error.java
@@ -2,25 +2,19 @@ package com.engalladofc.vector.model;
 
 public enum Error {
 
-    TITLE("Title cannot be empty", 1),
-    SUBJECT("Subject cannot be empty", 2),
-    DESCRIPTION("Max limit of 300 characters", 3),
-    DEADLINE("Invalid date Format (YYYY-MM-DD)", 4),
-    DIFFICULTY("Not in Range (1-5)", 5);
+    TITLE("Title cannot be empty"),
+    SUBJECT("Subject cannot be empty"),
+    DESCRIPTION("Max limit of 300 characters"),
+    DEADLINE("Invalid date Format (YYYY-MM-DD)"),
+    DIFFICULTY("Not in Range (1-5)");
 
     private String message;
-    private int num;
 
-    Error(String message, int num) {
+    Error(String message) {
         this.message = message;
-        this.num = num;
     }
 
     public String getMessage() {
         return message;
-    }
-
-    public int getNum() {
-        return num;
     }
 }
