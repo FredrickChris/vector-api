ba86446642740c7cfb5d301b1171b948c340958e
FredrickChris <engalladofcm@gmail.com>
2026-05-19 14:15:08 +0800

    Replaced ValidationResponse with SearchValidationResult

--- src/main/java/com/engalladofc/vector/dto/SearchValidationResult.java
+++ src/main/java/com/engalladofc/vector/dto/SearchValidationResult.java
@@ -1,27 +1,24 @@
 package com.engalladofc.vector.dto;
 
-import java.util.ArrayList;
+import java.util.List;
 import java.time.LocalDate;
 
-public class ValidationResponse {
-	private ArrayList<String> errors;
-	private String subject;
+public class SearchValidationResult {
+	private List<String> errors;
 	private LocalDate minDate;
 	private LocalDate maxDate;
 	private Integer minDiff;
 	private Integer maxDiff;
 	
-	public ValidationResponse(ArrayList<String> errors, String subject, LocalDate minDate, LocalDate maxDate, Integer minDiff, Integer maxDiff) {
+	public SearchValidationResult(List<String> errors, LocalDate minDate, LocalDate maxDate, Integer minDiff, Integer maxDiff) {
         this.errors = errors;
-        this.subject = subject;
         this.minDate = minDate;
         this.maxDate = maxDate;
         this.minDiff = minDiff;
         this.maxDiff = maxDiff;
 	}
 	
-	public ArrayList<String> getErrors() { return errors; }
-	public String getSubject() { return subject; }
+	public List<String> getErrors() { return errors; }
 	public LocalDate getMinDate() { return minDate; }
 	public LocalDate getMaxDate() { return maxDate; }
 	public Integer getMinDiff() { return minDiff; }
